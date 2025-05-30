8ee0843877fe7913a48e379c662223f132550368
jgdong25 <168694394+jgdong25@users.noreply.github.com>
2025-05-30 15:44:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -37,7 +37,6 @@ hysteria2://0e2e83d686da058b@34.80.123.201:26001?sni=www.bing.com&alpn=h3&insecu
 vless://41965c4e-24d4-4581-8575-0ef2a653ca4f@35.212.128.132:53262?encryption=none&security=reality&sni=www.lovelive-anime.jp&fp=chrome&pbk=g-oxbqigzCaXqARxuyD2_vbTYeMD9zn8wnTo02S69QM&type=tcp&headerType=none#Gcp300-US
 vless://6c92c4ee-ac48-4041-d4e6-caa38ef74045@34.150.1.91:40302?encryption=none&security=reality&sni=icloud.com&fp=chrome&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&type=tcp&headerType=none#Gcp300-HK
 hysteria2://5d04a3e89c95a534@34.150.1.91:26001?sni=www.bing.com&alpn=h3&insecure=1#HK-Google_Cloud
-hysteria2://edc314fc5e2fb7e8@163.223.183.64:60281?sni=www.bing.com&alpn=h3&insecure=1#US-Aiyun_HK_Network
 hysteria2://DRAAoEbDU6dRMrVKMNKEvhBrAY@8.210.184.183:6116?sni=bing.com&insecure=1#Claw-HK-12.6
 vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIlZscy1KUC1WdWx0ciIsDQogICJhZGQiOiAid3d3LnVkYWNpdHkuY29tIiwNCiAgInBvcnQiOiAiNDQzIiwNCiAgImlkIjogIjhkYjIxYmZiLTg1NjAtNGRmYS04ZWJjLWJlNDU1MmUwNmRhOSIsDQogICJhaWQiOiAiMCIsDQogICJzY3kiOiAibm9uZSIsDQogICJuZXQiOiAid3MiLA0KICAidHlwZSI6ICJub25lIiwNCiAgImhvc3QiOiAiZWd5cHQtc29ja2V0LWxlY3R1cmVzLWJlY29tZS50cnljbG91ZGZsYXJlLmNvbSIsDQogICJwYXRoIjogIi92bWVzcy1hcmdvP2VkPTIwNDgiLA0KICAidGxzIjogInRscyIsDQogICJzbmkiOiAiZWd5cHQtc29ja2V0LWxlY3R1cmVzLWJlY29tZS50cnljbG91ZGZsYXJlLmNvbSIsDQogICJhbHBuIjogIiIsDQogICJmcCI6ICIiDQp9
 hysteria2://ba498907-2c61-4161-ace1-09928193e744@113.29.231.226:27057?sni=www.bing.com&alpn=h3&insecure=1#hy2-ReadyServer
