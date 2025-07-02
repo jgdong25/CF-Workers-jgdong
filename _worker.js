fa14d86e8b3b58ea24ed14591c224dfe4cb1a12b
jgdong25 <168694394+jgdong25@users.noreply.github.com>
2025-07-02 14:52:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -24,6 +24,7 @@ vless://8224014b-696b-416f-f99c-00ab16e51dad@[2600:1900:41a0:44d::]:29238?encryp
 vless://6d47ffff-15b8-402b-a0a3-48f77818e2cf@[2600:1900:41a0:44d::]:13944?encryption=none&security=tls&sni=alice.321700.xyz&fp=chrome&type=ws&path=%2F#Alice-hk-tls-gcp%E4%B8%AD%E8%BD%AC
 
 vless://6d47ffff-15b8-402b-a0a3-48f77818e2cf@alice.321700.xyz:12166?encryption=none&security=tls&sni=alice.321700.xyz&fp=chrome&type=ws&path=%2F#Alice-hk-tls
+vless://8373437e-2f7e-42b7-8f51-b6dc3e98623d@[2600:1900:41a0:44d::]:40261?encryption=none&security=reality&sni=tesla.com&fp=chrome&pbk=8NH3TW4EGIpoHP_N3xzPD8KzWY27ZUXp2d-Fa9gtHWA&sid=51b365&spx=%2F&type=tcp&headerType=none#alice-hkv6-SG-clone
 hysteria2://6603a93f-650e-402e-b1d9-42668ac0424f@alice.321700.xyz:12168?sni=www.bing.com&alpn=h3&insecure=1#Alice-HKV6-hy2
 vless://b2692a1a-805d-4bd6-8068-7d8264965e16@104.19.0.0:2082?encryption=none&security=none&type=ws&host=cf.321700.xyz&path=%2F#yg-vl-US
 vless://fca2f5b6-1011-41b6-a822-7a6c6b24b168@claw7.321700.xyz:38116?encryption=none&security=reality&sni=icloud.com&fp=chrome&pbk=g1f1wLjim5gOVGnI5LGUV0dL4iFXPoiepOPZfSxJe14&type=tcp&headerType=none#claw7
