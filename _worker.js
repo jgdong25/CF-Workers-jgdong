4e0d7f009da9512778627d6096d04bb23c584e87
jgdong25 <168694394+jgdong25@users.noreply.github.com>
2025-07-08 10:13:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -27,8 +27,8 @@ vless://6d47ffff-15b8-402b-a0a3-48f77818e2cf@alice.321700.xyz:12166?encryption=n
 vless://8373437e-2f7e-42b7-8f51-b6dc3e98623d@[2600:1900:41a0:44d::]:40261?encryption=none&security=reality&sni=tesla.com&fp=chrome&pbk=8NH3TW4EGIpoHP_N3xzPD8KzWY27ZUXp2d-Fa9gtHWA&sid=51b365&spx=%2F&type=tcp&headerType=none#alice-hkv6-SG-clone
 hysteria2://6603a93f-650e-402e-b1d9-42668ac0424f@alice.321700.xyz:12168?sni=www.bing.com&alpn=h3&insecure=1#Alice-HKV6-hy2
 vless://b2692a1a-805d-4bd6-8068-7d8264965e16@104.19.0.0:2082?encryption=none&security=none&type=ws&host=cf.321700.xyz&path=%2F#yg-vl-US
-vless://b2692a1a-805d-4bd6-8068-7d8264965e16@[2602:fc59:b0:64::6811:19ad]:2082?encryption=none&security=none&type=ws&host=cf.321700.xyz&path=%2F#yg-vl-clone
-vless://61dc3dbf-b1a7-4fad-963f-0334053d22fd@[2001:67c:2960:6464::6815:e005]:443?encryption=none&security=tls&sni=bot.jgdong.dpdns.org&type=ws&host=bot.jgdong.dpdns.org&path=%2Fvless-argo%3Fed%3D2048#Vls-FI-Hetzner_Online-CDNV6
+vless://b2692a1a-805d-4bd6-8068-7d8264965e16@[2602:fc59:b0:64::6811:19ad]:2082?encryption=none&security=none&type=ws&host=cf.321700.xyz&path=%2F#yg-US-CDNV6
+vless://61dc3dbf-b1a7-4fad-963f-0334053d22fd@[2001:67c:2960:6464::6815:e005]:443?encryption=none&security=tls&sni=bot.jgdong.dpdns.org&type=ws&host=bot.jgdong.dpdns.org&path=%2Fvless-argo%3Fed%3D2048#Vls-FI-Hetzner-CDNV6
 
 vless://fca2f5b6-1011-41b6-a822-7a6c6b24b168@claw7.321700.xyz:38116?encryption=none&security=reality&sni=icloud.com&fp=chrome&pbk=g1f1wLjim5gOVGnI5LGUV0dL4iFXPoiepOPZfSxJe14&type=tcp&headerType=none#claw7
 //hysteria2://02af5ccb9bb66fe5@47.79.32.102:26002?sni=www.bing.com&alpn=h3&insecure=1#claw7-JP-hy2
