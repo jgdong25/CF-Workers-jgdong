8f6858b4d03dd2609e10356278f54ff8921da807
jgdong25 <168694394+jgdong25@users.noreply.github.com>
2025-07-22 11:27:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -42,8 +42,6 @@ hysteria2://ba498907-2c61-4161-ace1-09928193e744@113.29.231.226:27057?sni=www.bi
 ss://YWVzLTI1Ni1nY206d3JYRG0zdkNtY09Dd3E5dHdwdkRqMkxDb01LWXc3SER1OEtud3J6Q2hpRENuZ0hDdThLbWYwWERoVUJHdzdMRG5zS1N3cVREdFE9PQ%3D%3D@[240b:4000:93:de00:0:fe9:f9b0:2d94]:44331#SGP-Claw
 hysteria2://7675a824-9561-4a70-a69e-42a643b044ca@34.92.103.141:25168?sni=www.bing.com&alpn=h3&insecure=1#HK-Google_Cloud
 vless://d966970b-d375-47b7-b9d0-573c5a90562c@[2600:1900:41a0:44d::]:22044?type=tcp&security=reality&sni=www.icloud.com&pbk=UtL7E0Gmxj3X5JdcPAutpTRKo7K2hugkR0vwk2XroUM&flow=&fp=chrome#HK-Google-v6
-vless://eedb249c-f0a1-4e9a-8a58-3903ff037849@[2600:1900:41a0:44d::]:44418?encryption=none&security=tls&sni=alice1.321700.xyz&type=ws&host=alice1.321700.xyz&path=%2F#alice-cf-SG-GCP%E4%B8%AD%E8%BD%AC
-
 vless://4a1eae78-8a83-4d1c-936f-c6b3afcb5d9c@isif.987677.xyz:29184?encryption=none&flow=xtls-rprx-vision&security=reality&sni=icloud.com&fp=chrome&pbk=q9UhY5OjEbAZWmNxwzgRAq1CB7zDN7IvZ7FiAUI0IAg&sid=bf7ceec6&spx=%2F&type=tcp&headerType=none#isif_HK-solaireh
 
 `
