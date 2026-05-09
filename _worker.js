c54bfd42f2877d6c363cc3f364fe4019ec8fd358
jgdong25 <168694394+jgdong25@users.noreply.github.com>
2026-05-09 10:28:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let MainData = `
 vless://53f32066-a77f-4c00-94ba-b49a472dfdb6@162.159.49.189:443?encryption=none&security=tls&sni=buildings-julian-border-living.trycloudflare.com&insecure=0&allowInsecure=0&type=ws&host=buildings-julian-border-living.trycloudflare.com&path=%2F53f32066-a77f-4c00-94ba-b49a472dfdb6-vless%3Fed%3D2560#%F0%9F%87%AD%F0%9F%87%B0%20.alice
 vless://cfce222b-bb23-4904-bb42-32a358f881cd@accktw.321700.xyz:15767?encryption=none&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&insecure=0&allowInsecure=0&type=ws&path=%2F#acckvl
 hysteria2://f6f56414-694d-45af-8e65-a52d4a5394b3@accktw.321700.xyz:64557?sni=www.bing.com&alpn=h3&insecure=1&allowInsecure=1#accktwhy2
-vless://cfce222b-bb23-4904-bb42-32a358f881cd@64.81.114.103:25750?encryption=none&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&insecure=0&allowInsecure=0&type=ws&path=%2F#acck-tw-JP%E4%B8%AD%E8%BD%AC
+vless://9b5636fe-8989-4c54-bd69-0f651f2f7d27@47.238.224.188:8443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=learn.microsoft.com&fp=chrome&pbk=c9Vw8gjqHcjuoYSBNFrjYhpajg-RHuxe6wooSkF4uyA&sid=065e2985&type=tcp&headerType=none#localhost%20X-Reality
 
 vless://00a81285-47fb-40cf-fdef-a6af7cde2cf7@hdyjp.321700.xyz:58234?encryption=none&security=reality&sni=www.icloud.com&fp=chrome&pbk=qhTzYYIgBzDLNYR79oxftqdo1kzL-1_hGJKfqrOliCY&type=tcp&headerType=none#JP-%E8%83%A1%E8%92%82%E4%BA%91-VL
 hysteria2://b01d5748-4397-46c0-a954-0ba110cd31d2@hdyjp.321700.xyz:26001?sni=www.bing.com&alpn=h3&insecure=1&allowInsecure=1#JP-%E8%83%A1%E8%92%82%E4%BA%91
